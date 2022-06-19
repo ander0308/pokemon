d78b6e6795aa52a608c23a4fb08df84109560187
Anderson Santana <ander0308@yahoo.com.br>
2022-06-18 21:00:14 -0300

    chore: initial config

---
+++ js/all.js
@@ -0,0 +1,1 @@
+"use strict";console.log("buttons"),console.log("modal");var swiper=new Swiper(".slider-portfolio",{slidesPerView:1,spaceBetween:10,pagination:{el:".s-portfolio .container .swiper-pagination",clickable:!0}});
---
+++ js/scripts/swiper.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".slider-portfolio", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".s-portfolio .container .swiper-pagination",
+    clickable: true,
+  },
+
+});
